f6a522a5d9963794ba62010c1a2280a483e9a805
Adam Miller <chadam189@gmail.com>
2017-10-11 12:16:35 -0700

    Pass specs for functional pattern of both stack and queue

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -2,17 +2,34 @@ var Queue = function() {
   var someInstance = {};
 
   // Use an object with numeric keys to store values
-  var storage = {};
+  var storage = {
+    size: 0
+  };
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    storage.size++;
+    var newIndex = String(storage.size);
+    storage[newIndex] = value;
   };
 
   someInstance.dequeue = function() {
+    if (storage.size === 0) {
+      return 'Queue is empty';
+    } else {
+      var returnedValue = storage['1'];
+      for (var i = 1; i < storage.size; i++) {
+        storage[String(i)] = storage[String(i + 1)];
+      }
+      delete storage[String(storage.size)];
+      storage.size--;
+      return returnedValue;
+    }
   };
 
   someInstance.size = function() {
+    return storage.size;
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -2,16 +2,29 @@ var Stack = function() {
   var someInstance = {};
 
   // Use an object with numeric keys to store values
-  var storage = {};
+  var storage = {
+    size: 0
+  };
 
   // Implement the methods below
   someInstance.push = function(value) {
+    storage.size++;
+    var newIndex = String(storage.size);
+    storage[newIndex] = value;
   };
 
   someInstance.pop = function() {
+    if (storage.size === 0) {
+      return 'Stack is empty';
+    } else {
+      var returnedIndex = String(storage.size);
+      storage.size--;
+      return storage[returnedIndex];
+    }
   };
 
   someInstance.size = function() {
+    return storage.size;
   };
 
   return someInstance;
