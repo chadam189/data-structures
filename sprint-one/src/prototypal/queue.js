db17e8da54e1a9d6c8e6f5f56a4f26da25e193da
chadam189 <chadam189@gmail.com>
2017-10-11 20:39:03 -0700

    Pass the spec for prototypal patterns for stack and queue

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,33 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var newQueue = Object.create(queueMethods);
+  newQueue.queueSize = 0;
+  return newQueue;
 };
 
 var queueMethods = {};
+
+queueMethods.enqueue = function(value) {
+  this.queueSize++;
+  this[this.queueSize.toString()] = value;
+};
+
+queueMethods.dequeue = function() {
+  if (this.queueSize === 0) {
+    return 'queue is empty';
+  }
+  var dequeueVal = this['1'];
+  for (var i = 1; i < this.queueSize; i++) {
+    this[i.toString()] = this[String(i + 1)];
+  }
+  delete this[this.queueSize.toString()];
+  this.queueSize--;
+  return dequeueVal;
+};
+
+queueMethods.size = function() {
+  return this.queueSize;
+};
 
 
