e4a9d2c6c91abb75a95cb8c30ab93bd1f44cdaa4
chadam189 <chadam189@gmail.com>
2017-10-12 10:57:38 -0700

    Pass all provided tests for linked list

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,45 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    if (this.tail !== null) {
+      this.tail.next = Node(value);
+      this.tail = this.tail.next;
+    } else {
+      this.tail = Node(value);
+    }
+    if (this.head === null) {
+      this.head = this.tail;
+    }
+
   };
 
   list.removeHead = function() {
+    if (this.head === null) {
+      return 'Linked list is empty.';
+    }
+    var removeHeadVal = this.head.value;
+    this.head = this.head.next;
+    return removeHeadVal;
   };
 
   list.contains = function(target) {
+    // when linked list has multiple items
+    if (this.head !== this.tail) {
+      var currentNode = this.head;
+      while (currentNode !== null) {
+        if (currentNode.value === target) {
+          return true;
+        }
+        currentNode = currentNode.next;
+      }
+      return false;
+    // else when linked list has one item  
+    } else if (this.head.value !== undefined) {
+      return target === this.head.value;
+    // else when linked list is empty
+    } else {
+      return false;
+    }
   };
 
   return list;
