d85730a635bd4aa7eee28f4b3e4c8a99f403e992
chadam189 <chadam189@gmail.com>
2017-10-12 20:36:13 -0700

    Pass all provided tests for sets

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,24 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = {}; // fix me
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  var keyName = String(item);
+  this._storage[keyName] = 0;
 };
 
 setPrototype.contains = function(item) {
+  var keyName = String(item);
+  return this._storage.hasOwnProperty(keyName);
 };
 
 setPrototype.remove = function(item) {
+  var keyName = String(item);
+  delete this._storage[keyName];
 };
 
 /*
